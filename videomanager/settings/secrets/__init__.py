2c0cc80e0ce9181f507de99c7269909eabce9527
Mohit Aggarwal <mohitagg@gmail.com>
2016-11-03 01:14:42 -0700

    first commit

---
+++ videomanager/settings/secrets/__init__.py
@@ -0,0 +1,6 @@
+# add secret settings per environment e.g. development.py in this folder
+# template:
+'''
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '...'
+'''
---
+++ videomanager/settings/staging.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from .base import *
+from .secrets.staging import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
