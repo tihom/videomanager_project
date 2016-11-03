bbdb33020f257831cf1f1cff036ac94a86fdac38
Mohit Aggarwal <mohitagg@gmail.com>
2016-11-03 02:40:11 -0700

    added content blocks in base html

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -23,5 +23,11 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
     def test_h1_css(self):
         self.browser.get(self.get_full_url("home"))
         h1 = self.browser.find_element_by_tag_name("h1")
-        self.assertEqual(h1.value_of_css_property("color"), 
+        self.assertEqual(h1.value_of_css_property("color"),
                          "rgba(200, 50, 255, 1)")
+
+    def test_home_files(self):
+        self.browser.get(self.live_server_url + "/robots.txt")
+        self.assertNotIn("Not Found", self.browser.title)
+        self.browser.get(self.live_server_url + "/humans.txt")
+        self.assertNotIn("Not Found", self.browser.title)
