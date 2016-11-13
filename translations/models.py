7430b6df75e705ab14317e3ffe9d45a4e3d082f3
Mohit Aggarwal <mohitagg@gmail.com>
2016-11-13 13:20:39 -0800

    added config files for appengine

--- translations/models.py
+++ translations/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class VideoMeta(models.Model):
-    youtube_url = models.CharField(max_length=1024, unique=True)
-    title = models.CharField(max_length=1024)
+    youtube_url = models.CharField(unique=True, max_length=255)
+    title = models.CharField(max_length=255)
     description = models.TextField()
 
     def __str__(self):
@@ -11,12 +11,12 @@ class VideoMeta(models.Model):
 
 
 class OriginalVideo(models.Model):
-    sno = models.CharField(max_length=1024)
-    url = models.CharField(max_length=1024)
-    domain = models.CharField(max_length=1024)
-    subject = models.CharField(max_length=1024)
-    topic = models.CharField(max_length=1024)
-    tutorial = models.CharField(max_length=1024)
+    sno = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    domain = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    topic = models.CharField(max_length=255)
+    tutorial = models.CharField(max_length=255)
     priority = models.IntegerField(blank=True)
     # has one video meta object
     video_meta = models.ForeignKey(VideoMeta, on_delete=models.CASCADE, blank=True, null=True)
--- videomanager/settings/production.py
+++ videomanager/settings/production.py
@@ -4,5 +4,5 @@ from .secrets.production import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-#TODO: change this to domain once finalzied
+# TODO: change this to domain once finalzied
 ALLOWED_HOSTS = ["*"]
